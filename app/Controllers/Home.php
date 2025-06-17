bbdfe3e3561530de0c86dc0ffcf8f49622e3593b
AndyDanian <danianescobarv@gmail.com>
2025-06-17 01:44:26 -0300

    articulando titulo del header (app/Views/front/head_viewer.php) con el controlador app/Controler/Home.pho

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,42 +6,48 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo view('front/head_view');
+	$data['titulo']='inicio';
+        echo view('front/head_view',$data);
         echo view('front/navbar_view');
         echo view('front/principal');
         echo view('front/footer_view');
     }
     public function inicio()
     {
-        echo view('front/head_view');
+	$data['titulo']='inicio';
+        echo view('front/head_view',$data);
         echo view('front/navbar_view');
         echo view('front/principal');
         echo view('front/footer_view');
     }
     public function acerca()
     {
-        echo view('front/head_view');
+	$data['titulo']='acerca de nosotros';
+        echo view('front/head_view',$data);
         echo view('front/navbar_view');
         echo view('front/acerca');
         echo view('front/footer_view');
     }
     public function quienes()
     {
-        echo view('front/head_view');
+	$data['titulo']='quienes somos';
+        echo view('front/head_view',$data);
         echo view('front/navbar_view');
         echo view('front/quienes');
         echo view('front/footer_view');
     }
     public function registrar()
     {
-        echo view('front/head_view');
+	$data['titulo']='registro';
+        echo view('front/head_view',$data);
         echo view('front/navbar_view');
         echo view('front/registrar');
         echo view('front/footer_view');
     }
     public function acceder()
     {
-        echo view('front/head_view');
+	$data['titulo']='acceder';
+        echo view('front/head_view',$data);
         echo view('front/navbar_view');
         echo view('front/acceder');
         echo view('front/footer_view');
